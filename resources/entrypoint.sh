e7b43640f7fb35a6c08aff611c76970c4876d71d
kimisiah <jerrick.m.falogme@accenture.com>
2017-07-19 16:48:15 +0800

    Update entrypoint.sh

--- resources/entrypoint.sh
+++ resources/entrypoint.sh
@@ -1,19 +1,18 @@
 #!/bin/bash
 
-echo "Genarate JENKINS SSH KEY and add it to gerrit"
-whoami
-host=$GERRIT_HOST_NAME
-port=$GERRIT_PORT
-gerrit_provider_id="adop-gerrit"
-gerrit_protocol="ssh"
-username=$GERRIT_JENKINS_USERNAME
-password=$GERRIT_JENKINS_PASSWORD
-nohup /usr/share/jenkins/ref/adop\_scripts/generate_key.sh -c ${host} -p ${port} -u ${username} -w ${password} &
+#echo "Genarate JENKINS SSH KEY and add it to gerrit"
+#host=$GERRIT_HOST_NAME
+#port=$GERRIT_PORT
+#gerrit_provider_id="adop-gerrit"
+#gerrit_protocol="ssh"
+#username=$GERRIT_JENKINS_USERNAME
+#password=$GERRIT_JENKINS_PASSWORD
+#nohup /usr/share/jenkins/ref/adop\_scripts/generate_key.sh -c ${host} -p ${port} -u ${username} -w ${password} &
 
 echo "Setting up your default SCM provider - Gerrit..."
 whoami
-mkdir -p $PLUGGABLE_SCM_PROVIDER_PROPERTIES_PATH $PLUGGABLE_SCM_PROVIDER_PATH
-mkdir -p ${PLUGGABLE_SCM_PROVIDER_PROPERTIES_PATH}/CartridgeLoader ${PLUGGABLE_SCM_PROVIDER_PROPERTIES_PATH}/ScmProviders
+#mkdir -p $PLUGGABLE_SCM_PROVIDER_PROPERTIES_PATH $PLUGGABLE_SCM_PROVIDER_PATH
+#mkdir -p ${PLUGGABLE_SCM_PROVIDER_PROPERTIES_PATH}/CartridgeLoader ${PLUGGABLE_SCM_PROVIDER_PROPERTIES_PATH}/ScmProviders
 nohup /usr/share/jenkins/ref/adop\_scripts/generate_gerrit_scm.sh -i ${gerrit_provider_id} -p ${gerrit_protocol} -h ${host} &
 
 echo "Tokenising scriptler scripts..."
@@ -26,5 +25,5 @@ echo "2.7.4" > /var/jenkins_home/jenkins.install.UpgradeWizard.state
 echo "start JENKINS"
 whoami
 
-chown -R 1000:1000 /var/jenkins_home
-su jenkins -c /usr/local/bin/jenkins.sh
+#chown -R 1000:1000 /var/jenkins_home
+/usr/local/bin/jenkins.sh
